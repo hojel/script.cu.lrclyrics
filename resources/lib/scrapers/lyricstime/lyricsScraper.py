6db9d2391061bba40edbb1672d92e4fa02ad8d2e
ronie <ronie@poedel.net>
2012-11-25 20:09:10 +0100

    fix broken lyricstime scraper

--- resources/lib/scrapers/lyricstime/lyricsScraper.py
+++ resources/lib/scrapers/lyricstime/lyricsScraper.py
@@ -5,8 +5,14 @@ import re
 from utilities import *
 
 __title__ = 'lyricstime'
-__priority__ = '210'
+__priority__ = '220'
 __lrc__ = False
+
+def replace_char(string):
+    invalid_char = [" ",",","'","&","and"]
+    for char in invalid_char:
+        string = string.replace(char,"-")
+    return string
 
 class LyricsFetcher:
     def __init__( self ):
@@ -18,10 +24,10 @@ class LyricsFetcher:
     def get_lyrics(self, artist, song):
         log( "%s: searching lyrics for %s - %s" % (__title__, artist, song))
         try: # ***** parser - changing this changes search string
-            url = "http://www.lyricstime.com/%s-%s-lyrics.html" % (replace(artist.lower().replace(" ","-").replace("---","-").replace("--","-")),replace(song.lower().replace(" ","-").replace("---","-").replace("--","-")))
+            url = "http://www.lyricstime.com/%s-%s-lyrics.html" % (replace_char(artist.lower()).replace("---","-").replace("--","-"),replace_char(song.lower()).replace("---","-").replace("--","-"))
             song_search = urllib.urlopen(url).read()
             log( "%s: search url: %s" % (__title__, url))
-            lyr = song_search.split('<div id="songlyrics" >')[1].split('</div>')[0]
+            lyr = song_search.split('<div id="songlyrics" style="padding-right:20px;">')[1].split('</div>')[0]
             lyr = self.clean_br_regex.sub( "\n", lyr ).strip()
             lyr = self.clean_lyrics_regex.sub( "", lyr ).strip()
             lyr = self.normalize_lyrics_regex.sub(
@@ -30,9 +36,5 @@ class LyricsFetcher:
             lyrics = self.clean_info_regex.sub( "", lyr )
             return lyrics, False
         except:
-            log( "%s: %s::%s (%d) [%s]" % ( __title__, self.__class__.__name__,
-                                        sys.exc_info()[ 2 ].tb_frame.f_code.co_name,
-                                        sys.exc_info()[ 2 ].tb_lineno,
-                                        sys.exc_info()[ 1 ]
-                                        ))
+            log( "%s: %s::%s (%d) [%s]" % ( __title__, self.__class__.__name__, sys.exc_info()[ 2 ].tb_frame.f_code.co_name, sys.exc_info()[ 2 ].tb_lineno, sys.exc_info()[ 1 ] ))
             return None, False
